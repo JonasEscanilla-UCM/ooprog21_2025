dd40b01be479784dbd2b98b5c4b08d07ac284edc
JonasEscanilla-UCM <nash.escanilla123@gmail.com>
2025-09-02 19:34:26 +0800

    Update Comments.java

--- Chapter_1/Comments.java
+++ Chapter_1/Comments.java
@@ -1,16 +1,25 @@
 /**
  * Javadoc Comments
- * Javadoc Comments
+ * This is my OOProg Exercise 4
  * Javadoc Comments
  */
 public class Comments {
     public static void main(String[] args) {
         //Single-line Comment
+        // System.out.println("HIGH HOPES [Panic!At The Disco]");
 
         /*
          Multi-line Comments
-         Multi-line Comments
-         Multi-line Comments
+         for (int i = 0; i < 2; i++) {
+            System.out.println("X                         X");
+        }
+            System.out.println("X       XXXXXXXXXX        X");
+            System.out.println("XXXXX   X        X    XXXXX");
+
+        for (int i = 0; i < 2; i++) {
+            System.out.println("X   X   X        X    X   X");
+        }
         */
     }
 }
+
