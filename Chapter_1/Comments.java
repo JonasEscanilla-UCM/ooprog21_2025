f864060d104bc63ac43e3ea09af91d212adf2933
JonasEscanilla-UCM <nash.escanilla123@gmail.com>
2025-09-02 14:30:42 +0800

    Add files via upload

---
+++ Chapter_1/Comments.java
@@ -0,0 +1,16 @@
+/**
+ * Javadoc Comments
+ * Javadoc Comments
+ * Javadoc Comments
+ */
+public class Comments {
+    public static void main(String[] args) {
+        //Single-line Comment
+
+        /*
+         Multi-line Comments
+         Multi-line Comments
+         Multi-line Comments
+        */
+    }
+}
---
+++ Chapter_1/FavoriteSong.java
@@ -0,0 +1,13 @@
+public class FavoriteSong {
+    public static void main(String[] args) {
+        System.out.println("HIGH HOPES [Panic!At The Disco]");
+        System.out.println("Had to have high, high hopes for a living");
+        System.out.println("Shooting for the stars when I couldn't make a killing");
+        System.out.println("Didn't have a dime, but I always had a vision");
+        System.out.println("Always had high, high hopes (High, high hopes)");
+        System.out.println("Had to have high, high hopes for a living");
+        System.out.println("Didn't know how, but I always had a feeling");
+        System.out.println("I was gonna be that one in a million");
+        System.out.println("Always had high, high hopes");
+    }
+}
