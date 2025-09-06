ec90b03655724099a0dfc580f6bdb45766be574b
JonasEscanilla-UCM <nash.escanilla123@gmail.com>
2025-09-06 11:05:17 +0800

    Add files via upload

---
+++ Chapter_1/FixDebugOne.java
@@ -0,0 +1,16 @@
+public class FixDebugOne {
+//    This program displays a greeting
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
+/*
+List down the spotted bugs below:
+Bugs:
+1. class name has space
+2. doesn't have closing curly braces at the end of the class
+3. Typo. "Systm" instead of "System"
+4. "System.out.println("Hello World!")" has no ';'
+5. method name is wrong. "Main" instead of "main"
+6. parameter is wrong. "(String args)" should be (String[] args)
+*/
