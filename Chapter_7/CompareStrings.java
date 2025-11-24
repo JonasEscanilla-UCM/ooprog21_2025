9377f87661021bed0fcd63346cf3bcb5f6fc2a11
JonasEscanilla-UCM <nash.escanilla123@gmail.com>
2025-11-25 07:52:18 +0800

    Update CompareStrings.java

--- Chapter_7/CompareStrings.java
+++ Chapter_7/CompareStrings.java
@@ -1,5 +1,4 @@
 import java.util.Scanner;
-
 public class CompareStrings {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
@@ -18,3 +17,4 @@ public class CompareStrings {
         sc.close();
     }
 }
+
